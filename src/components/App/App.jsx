dd959fac00c2cabe7de60736333859b9f01055db
Margaryta Dieltsova <112723193+MargaritaDel@users.noreply.github.com>
2023-04-21 22:20:21 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,18 +5,37 @@ import Filter from '../Filter/Filter';
 import ContactList from '../ContactsList/ContactList';
 import { Container } from './App.styled';
 
+const initialContacts = [
+  { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+  { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+  { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+  { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+];
 
 export class App extends Component {
   state = {
 
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      const parsedContacts = JSON.parse(savedContacts);
+      this.setState({ contacts: parsedContacts });
+    } else {
+      this.setState({ contacts: initialContacts });
+    }
+    
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+ 
 
   addContact = (name, number) => {
     !this.state.contacts.find(
@@ -49,21 +68,8 @@ export class App extends Component {
     );
 
 
-    componentDidMount() {
-      const savedContacts = localStorage.getItem('contacts');
-      if (savedContacts !== null) {
-        const parsedContacts = JSON.parse(savedContacts);
-        this.setState({ contacts: parsedContacts });
-      } else {
-        this.setState({ contacts: this.contacts });
-      }
-    }
   
-    componentDidUpdate(_, prevState) {
-      if (prevState.contacts !== this.state.contacts) {
-        localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-      }
-    }
+    
 
   render() {
     const contacts = this.getFilterContacts();
